2f78bc3ac58c6062ed1de5304374509f252bc6e0
samihasher <you@example.com>
2023-05-15 22:40:02 -0400

    updated read me and index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 console.log('hello world');
 
 
+let firstname = "samiha"
+let lastname = "sher"
+let finalString = firstname + "," + " " + lastname
+console.log('finalString:', finalString);
+
+console.log('\n===\n');
+
