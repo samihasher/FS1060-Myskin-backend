ae8674c5e3f409f5ecad91c44b235f66e6c5563b
samihasher <you@example.com>
2023-06-21 18:55:49 -0400

    added 2 modules, routines and users

---
+++ src/routines.js
@@ -0,0 +1,28 @@
+// Jane's AM Skincare routine
+export const janeAMSkincare = [
+    "Cleanser",
+    "Toner",
+    "Serum",
+    "Moisturizer",
+    "Sunscreen",
+  ];
+  
+  // John's PM Skincare routine
+  export const janePMSkincare = [
+    "Cleanser",
+    "Toner",
+    "Serum",
+    "Moisturizer",
+  ];
+  
+  // John's AM Skincare routine
+  export const johnAMSkincare = [
+    "Cleanser",
+    "Toner",
+    "Serum",
+    "Moisturizer",
+    "Sunscreen",
+  ];
+
+  
+  
