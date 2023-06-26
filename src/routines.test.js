9ad3df093d741234ff28f18bfd936f5a824b309f
samihasher <you@example.com>
2023-06-26 19:08:16 -0400

    updated routine.test for relative path

--- src/routines.test.js
+++ src/routines.test.js
@@ -2,15 +2,15 @@ import {
     janeAMSkincare,
     janePMSkincare,
     johnAMSkincare,
-  } from "../routines.js";
+  } from "./routines.js";
   
   describe("Skincare Routines", () => {
     test("Jane's AM Skincare Routine should have 5 steps", () => {
       expect(janeAMSkincare).toHaveLength(5);
     });
   
-    test("Jane's PM Skincare Routine should have 6 steps", () => {
-      expect(janePMSkincare).toHaveLength(6);
+    test("Jane's PM Skincare Routine should have 4 steps", () => {
+      expect(janePMSkincare).toHaveLength(4);
     });
   
     test("John's AM Skincare Routine should have 5 steps", () => {
