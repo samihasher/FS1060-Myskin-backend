c8690163d00b8a4cffe4c19832eafb52fea8fa16
Samiha Sher <122646965+samihasher@users.noreply.github.com>
2023-06-26 18:37:07 -0400

    Update routinestest.js

--- src/routinestest.js
+++ src/routinestest.js
@@ -1,15 +1,15 @@
 import {
     janeAMSkincare,
-    johnPMSkincare,
+    janePMSkincare,
     johnAMSkincare,
-  } from "../skincareroutines.js";
+  } from "../routines.js";
   
   describe("Skincare Routines", () => {
     test("Jane's AM Skincare Routine should have 5 steps", () => {
       expect(janeAMSkincare).toHaveLength(5);
     });
   
-    test("John's PM Skincare Routine should have 6 steps", () => {
+    test("Jane's PM Skincare Routine should have 6 steps", () => {
       expect(johnPMSkincare).toHaveLength(6);
     });
   
